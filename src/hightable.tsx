6b0a520322ebbe75b49f467e6843f7fda3c4c06a
Kenny Daniel <platypii@gmail.com>
2024-05-27 17:10:31 -0700

    Add simple table component

---
+++ src/hightable.tsx
@@ -0,0 +1,54 @@
+interface TableProps {
+  header: string[]
+  data?: any[][]
+}
+
+/**
+ * Render a table
+ */
+export default function HighTable({ header, data }: TableProps) {
+  if (!header.length) return
+
+  /**
+   * Validate row length
+   */
+  function rowError(row: any[], rowIndex: number): string | undefined {
+    if (row.length !== header.length) {
+      return `Row ${rowIndex + 1} length does not match header`
+    }
+    return undefined
+  }
+
+  return <div className='outer'>
+    <div style={{ overflow: 'auto' }}>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th></th>
+            {header.map((row, index) => (
+              <th key={index} style={{ maxWidth: 'inherit' }}>{row}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data && data.map((row, rowIndex) => (
+            <tr key={rowIndex} title={rowError(row, rowIndex)}>
+              <td>{(rowIndex + 1).toLocaleString()}</td>
+              {Array.from(row).map((columnValue, columnIndex) => (
+                <td key={columnIndex} style={{ maxWidth: 'inherit' }} title={title(columnValue)}>
+                  {columnValue?.toString()}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    <div className='tableCorner'>&nbsp;</div>
+  </div>
+}
+
+function title(value: any): string | undefined {
+  const str = value?.toString()
+  if (str?.length > 20) return str
+}
