f53967e0f414ad59a4503729c550f20a56cfce07
Kenny Daniel <platypii@gmail.com>
2024-06-02 03:18:27 -0700

    Magical require function

--- demo.js
+++ demo.js
@@ -13,7 +13,7 @@ const data = {
 
 // Load ./src/hightable.tsx and render
 function init() {
-  const HighTable = require('./src/hightable.js').default
+  const HighTable = require('./src/hightable.tsx').default
   const container = document.getElementById('app')
   const root = ReactDOM.createRoot(container)
   root.render(React.createElement(HighTable, { data }))
@@ -21,40 +21,42 @@ function init() {
 init()
 
 /**
- * Fake require function to load modules
- *
- * @param {string} path
- * @returns {any}
- */
-function require(path) {
-  if (path === 'react') return React
-  if (path === 'react-dom') return ReactDOM
-  if (path === './src/hightable.js') return requireUrl('hightable.tsx')
-  if (path === './tableheader.js') return requireUrl('tableheader.tsx')
-  throw new Error(`Cannot find module '${path}'`)
-}
-
-/**
+ * Magical require function to load modules and typescript from URLs.
  * Load source code from URL and compile with babel.
  * Needs to be synchronous for require() to work.
  * Credit: https://github.com/hrgdavor/jscadui/tree/main/packages/require
  *
- * @param {string} filename
- * @returns {string}
+ * @param {string} url
+ * @returns {any}
  */
-function requireUrl(filename) {
+function require(url) {
+  if (url === 'react') return React
+  if (url === 'react-dom') return ReactDOM
+  if (url === './src/tableheader.js') url = './src/tableheader.tsx'
+
+  const filename = url.replace(/^\.\//, '')
   const req = new XMLHttpRequest()
-  req.open('GET', `./src/${filename}`, false) // sync
+  req.open('GET', url, false) // sync
   req.send()
   if (req.status !== 200) throw new Error(`Failed to load ${filename}`)
   const text = req.responseText
 
-  // compile with babel
+  // compile with babel, sourceURL for debugging
   const js = Babel.transform(text, { filename, presets: ['env', 'react', 'typescript'] }).code
     + `\n//# sourceURL=${filename}`
+
+  // require relative to current url
+  function requireRelative(path) {
+    if (path.startsWith('./')) {
+      // resolve relative path
+      path = url.replace(/[^/]+$/, '') + path.slice(2)
+    }
+    return require(path)
+  }
+
   // eval with custom require
   const run = eval('(require, exports, source) => eval(source)')
   const exports = {}
-  run(require, exports, js)
+  run(requireRelative, exports, js)
   return exports
 }
