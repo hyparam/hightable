4357851204a0c3a0b5da9e698f925058486b5428
Sylvain Lesage <severo@rednegra.net>
2025-02-10 19:07:30 +0100

    add tests

--- test/promise.test.ts
+++ test/promise.test.ts
@@ -3,13 +3,31 @@ import { resolvablePromise } from '../src/promise.js'
 
 describe('resolvablePromise', () => {
   it('should resolve with a value', async () => {
-    const { promise, resolve } = resolvablePromise<number>()
-    resolve(42)
-    await expect(promise).resolves.toBe(42)
+    const wrapped = resolvablePromise<number>()
+    wrapped.resolve(42)
+
+    // the resolved value is not in the object yet
+    expect(wrapped.resolved).toBe(undefined)
+
+    // await the promise to get the value
+    await expect(wrapped.promise).resolves.toBe(42)
+
+    // the resolved value is stored in the wrapped object
+    expect(wrapped.resolved).toBe(42)
+    expect(wrapped.rejected).toBe(undefined)
   })
   it('should reject with an error', async () => {
-    const { promise, reject } = resolvablePromise<number>()
-    reject(new Error('Failed'))
-    await expect(promise).rejects.toThrow('Failed')
+    const wrapped = resolvablePromise<number>()
+    wrapped.reject(new Error('Failed'))
+
+    // the rejected error is not in the object yet
+    expect(wrapped.rejected).toBe(undefined)
+
+    // await the promise to get the error
+    await expect(wrapped.promise).rejects.toThrow(new Error('Failed'))
+
+    // the rejected error is stored in the wrapped object
+    expect(wrapped.resolved).toBe(undefined)
+    expect(wrapped.rejected).toBeInstanceOf(Error)
   })
 })
