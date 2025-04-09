a61cb8afe8acaac06175f550d6755836722ebf3f
Sylvain Lesage <severo@rednegra.net>
2025-04-09 09:42:46 +0200

    remove obsolete export

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ export type { AsyncRow, Cells, PartialRow, ResolvableRow, Row } from './helpers/
 export { rowCache } from './helpers/rowCache.js'
 export type { Selection } from './helpers/selection.js'
 export type { OrderBy } from './helpers/sort.js'
-export type { Config } from './hooks/useConfig.js'
 export { resolvablePromise, wrapPromise, wrapResolved } from './utils/promise.js'
 export type { ResolvablePromise } from './utils/promise.js'
 export { stringify } from './utils/stringify.js'
