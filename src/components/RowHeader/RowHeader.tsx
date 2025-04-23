cbe3bdf16451c1fb767af5b0e0da35ab90aecacb
Sylvain Lesage <severo@rednegra.net>
2025-04-23 17:22:09 +0200

    show a placeholder (blurred number) while row index is loading

--- src/components/RowHeader/RowHeader.tsx
+++ src/components/RowHeader/RowHeader.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties, MouseEvent, ReactNode } from 'react'
 
 interface Props {
+  busy?: boolean
   checked?: boolean
   children?: ReactNode
   onClick?: (event: MouseEvent) => void
@@ -8,10 +9,10 @@ interface Props {
   style?: CSSProperties
 }
 
-export default function RowHeader({ children, checked, onClick, showCheckBox, style }: Props) {
+export default function RowHeader({ children, checked, onClick, showCheckBox, style, busy }: Props) {
   const disabled = !onClick
   return (
-    <th scope="row" role="rowheader" style={style} onClick={onClick}>
+    <th scope="row" role="rowheader" style={style} onClick={onClick} aria-busy={busy}>
       <span>{children}</span>
       { showCheckBox && <input type='checkbox' disabled={disabled} checked={checked} readOnly /> }
     </th>
