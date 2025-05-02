95a45ec028b3fc9110481a1b55bc2de03c350bed
Sylvain Lesage <severo@rednegra.net>
2025-05-02 18:49:29 +0200

    add support for resizing with keyboard

--- src/components/ColumnResizer/ColumnResizer.tsx
+++ src/components/ColumnResizer/ColumnResizer.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useCallback, useEffect, useState } from 'react'
+import { KeyboardEvent, MouseEvent, useCallback, useEffect, useState } from 'react'
 
 interface Props {
   onDoubleClick?: () => void
@@ -8,8 +8,11 @@ interface Props {
   navigateToCell?: () => void
 }
 
+const keyboardShiftWidth = 10
+
 export default function ColumnResizer({ onDoubleClick, setWidth, width, tabIndex, navigateToCell }: Props) {
   const [resizeClientX, setResizeClientX] = useState<number | undefined>(undefined)
+  const [activeKeyboard, setActiveKeyboard] = useState<boolean>(false)
 
   const handleDoubleClick = useCallback(() => {
     navigateToCell?.()
@@ -63,16 +66,60 @@ export default function ColumnResizer({ onDoubleClick, setWidth, width, tabIndex
     }
   }, [resizeClientX, setWidth])
 
+  const onFocus = useCallback(() => {
+    setActiveKeyboard(true)
+  }, [])
+
+  const onBlur = useCallback(() => {
+    setActiveKeyboard(false)
+  }, [])
+
+  const onKeyDown = useCallback((e: KeyboardEvent) => {
+    if (!activeKeyboard) {
+      // let the event propagate to the parent
+      return
+    }
+    e.stopPropagation()
+    if (resizeClientX !== undefined) {
+      // don't allow keyboard events when resizing with the mouse
+      return
+    }
+    if (e.key === 'Escape') {
+      // cancel resizing and focus the parent cell
+      setActiveKeyboard(false)
+      navigateToCell?.()
+      return
+    }
+    if (e.key === 'Enter' || e.key === ' ') {
+      // autoresize
+      onDoubleClick?.()
+      return
+    }
+    if (width === undefined) {
+      // don't allow other keyboard events when width is not set
+      return
+    }
+    if (e.key === 'ArrowRight') {
+      setWidth?.(Math.max(1, width + keyboardShiftWidth))
+    } else if (e.key === 'ArrowLeft') {
+      setWidth?.(Math.max(1, width - keyboardShiftWidth))
+    }
+  }, [onDoubleClick, resizeClientX, setWidth, width, activeKeyboard, navigateToCell])
+
+  const ariaBusy = resizeClientX !== undefined || activeKeyboard
+
   return (
     <span
       role="separator"
       aria-orientation="vertical"
-      aria-busy={resizeClientX !== undefined}
-      // TODO: make it focusable + keyboard accessible and add aria properties (https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Roles/separator_role)
+      aria-busy={ariaBusy}
       // Note that aria-valuenow would be helpful for tests.
       onDoubleClick={handleDoubleClick}
       onMouseDown={onMouseDown}
       onClick={disableOnClick}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onKeyDown={onKeyDown}
       tabIndex={tabIndex}
     />
   )
