3597421f9d2fe5013a67bf44bbc2ea1b52e2101b
Sylvain Lesage <severo@rednegra.net>
2025-05-19 18:00:40 +0200

    simplify the code - show the corner checkbox if the other row checkboxes are shown

--- src/components/HighTable/HighTable.tsx
+++ src/components/HighTable/HighTable.tsx
@@ -146,7 +146,6 @@ export function HighTableInner({
 
   const showSelection = selection !== undefined
   const showSelectionControls = showSelection && onSelectionChange !== undefined
-  const showCornerSelection = showSelectionControls || showSelection && areAllSelected({ ranges: selection.ranges, length: numRows })
   const getOnSelectAllRows = useCallback(() => {
     if (!selection) return
     const { ranges } = selection
@@ -465,7 +464,7 @@ export function HighTableInner({
                 <TableCorner
                   onClick={getOnSelectAllRows()}
                   checked={allRowsSelected}
-                  showCheckBox={showCornerSelection}
+                  showCheckBox={showSelection}
                   style={cornerStyle}
                   ariaColIndex={1}
                   ariaRowIndex={1}
