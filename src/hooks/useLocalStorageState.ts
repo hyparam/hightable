0041e6792b99283d3c356873b4460e5b9b09c35a
Sylvain Lesage <severo@rednegra.net>
2025-06-04 18:07:04 +0200

    update docstring to reflect change

--- src/hooks/useLocalStorageState.ts
+++ src/hooks/useLocalStorageState.ts
@@ -36,15 +36,13 @@ interface Options<T> {
 /**
  * Hook to use a state that is persisted in local storage.
  *
- * The initial value is loaded from local storage if the key is defined.
- * If the key is undefined, the local storage is not used, and the initial value is undefined.
+ * If the key is not defined, it's a normal useState hook. The only difference is that the initial value is always undefined.
+ *
+ * If the key is defined, the initial value is loaded from local storage, and the value is persisted in local storage after each change.
  *
  * If the key changes, the value is updated from local storage. If the new key is undefined, the value does not change.
- *
  * Note that the values stored with a previous key are maintained.
  * TODO(SL): add a way to delete them?
- *
- * Contrarily to useState, the initial value is always undefined.
  *
  * @param options
  * @param [string | undefined] options.key The key to use in local storage. If undefined, the value is not persisted.
