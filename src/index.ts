b6055748aec72792c9c0f6934524370a54ed118e
Sylvain Lesage <severo@rednegra.net>
2025-07-04 00:07:42 +0200

    re-enable legacy exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import HighTable from './components/HighTable/HighTable.js'
 export { arrayDataFrame, sortableDataFrame } from './helpers/dataframe/index.js'
 export type { DataFrame } from './helpers/dataframe/index.js'
-// export { asyncRows, awaitRow, awaitRows, resolvableRow } from './helpers/row.js'
-// export type { AsyncRow, Cells, PartialRow, ResolvableRow, Row } from './helpers/row.js'
-// export { rowCache } from './helpers/rowCache.js'
+export { resolvablePromise, wrapPromise, wrapResolved } from './helpers/dataframe/legacy/promise.js'
+export type { ResolvablePromise } from './helpers/dataframe/legacy/promise.js'
+export { asyncRows, awaitRow, awaitRows, resolvableRow } from './helpers/dataframe/legacy/row.js'
+export type { AsyncRow, Cells, PartialRow, ResolvableRow, Row } from './helpers/dataframe/legacy/row.js'
 export type { Selection } from './helpers/selection.js'
 export type { OrderBy } from './helpers/sort.js'
-// export { resolvablePromise, wrapPromise, wrapResolved } from './utils/promise.js'
-// export type { ResolvablePromise } from './utils/promise.js'
 export { stringify } from './utils/stringify.js'
 export { HighTable }
 export default HighTable
