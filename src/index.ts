5b9827d078420a6e6e38c131343846c87fa0d37b
Brian Bleakley <brianbleakley@gmail.com>
2025-07-15 12:10:59 -0700

    more exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import HighTable from './components/HighTable/HighTable.js'
-export { arrayDataFrame, filterDataFrame, sortableDataFrame } from './helpers/dataframe/index.js'
-export type { DataFrame } from './helpers/dataframe/index.js'
+export { arrayDataFrame, filterDataFrame, sortableDataFrame, convertV1ToDataFrame } from './helpers/dataframe/index.js'
+export type { DataFrame, DataFrameEvents } from './helpers/dataframe/index.js'
 export { resolvablePromise, wrapPromise, wrapResolved } from './helpers/dataframe/legacy/promise.js'
 export type { ResolvablePromise } from './helpers/dataframe/legacy/promise.js'
 export { asyncRows, awaitRow, awaitRows, resolvableRow } from './helpers/dataframe/legacy/row.js'
 export type { AsyncRow, Cells, PartialRow, ResolvableRow, Row } from './helpers/dataframe/legacy/row.js'
 export type { Selection } from './helpers/selection.js'
 export type { OrderBy } from './helpers/sort.js'
+export { createEventTarget } from './helpers/typedEventTarget.js'
+export type { CustomEventTarget, TypedCustomEvent } from './helpers/typedEventTarget.js'
 export { stringify } from './utils/stringify.js'
 export { HighTable }
 export default HighTable
