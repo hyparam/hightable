1c08e2ae3d51308438dadf3394844e736aeffcdb
Sylvain Lesage <severo@rednegra.net>
2025-08-22 15:11:06 -0400

    export more helpers (#260)

--- src/helpers/dataframe/index.ts
+++ src/helpers/dataframe/index.ts
@@ -1,6 +1,6 @@
 export { arrayDataFrame } from './array.js'
 export { filterDataFrame } from './filter.js'
-export { checkSignal, createGetRowNumber, validateColumn, validateFetchParams, validateRow } from './helpers.js'
+export { checkSignal, createGetRowNumber, validateColumn, validateFetchParams, validateGetCellParams, validateGetRowNumberParams, validateOrderBy, validateRow } from './helpers.js'
 export { convertV1ToDataFrame } from './legacy/convert.js'
 export { fetchIndexes, sortableDataFrame } from './sort.js'
 export type { Cells, ColumnDescriptor, DataFrame, DataFrameEvents, Fetch, Obj, ResolvedValue } from './types.js'
