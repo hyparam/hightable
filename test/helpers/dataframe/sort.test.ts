8eeae7b9ac64a2c96911b5110ebb33f6e3dca3e5
Sylvain Lesage <severo@rednegra.net>
2025-11-28 15:53:02 +0100

    lint

--- test/helpers/dataframe/sort.test.ts
+++ test/helpers/dataframe/sort.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { arrayDataFrame } from '../../../src/helpers/dataframe/array.js'
 import { sortableDataFrame } from '../../../src/helpers/dataframe/sort.js'
 
