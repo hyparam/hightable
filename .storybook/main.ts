acffa6f8734db47cd5ff402f577ef00cbce44f13
Sylvain Lesage <severo@rednegra.net>
2025-11-28 09:30:30 +0100

    remove unneeded config which wreates warnings in the console

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   framework: '@storybook/react-vite',
   stories: [
-    '../src/**/*.mdx',
     '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   core: {
