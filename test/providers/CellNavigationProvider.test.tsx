10d8ff80d2d19337dd98e8d771e8b1ce3a405839
Sylvain Lesage <severo@rednegra.net>
2025-12-19 15:28:44 +0100

    add tests

--- test/providers/CellNavigationProvider.test.tsx
+++ test/providers/CellNavigationProvider.test.tsx
@@ -3,9 +3,12 @@ import { useContext } from 'react'
 import { describe, expect, it } from 'vitest'
 
 import { CellNavigationContext } from '../../src/contexts/CellNavigationContext.js'
+import { ColumnVisibilityStatesContext } from '../../src/contexts/ColumnVisibilityStatesContext.js'
+import { DataContext } from '../../src/contexts/DataContext.js'
+import { arrayDataFrame } from '../../src/helpers/dataframe/index.js'
 import { CellNavigationProvider } from '../../src/providers/CellNavigationProvider.js'
 
-function TestComponent() {
+function RowCountComponent() {
   const { cellPosition, setRowIndex } = useContext(CellNavigationContext)
   return (
     <div>
@@ -14,41 +17,118 @@ function TestComponent() {
     </div>
   )
 }
+function ColCountComponent() {
+  const { cellPosition, setColIndex } = useContext(CellNavigationContext)
+  return (
+    <div>
+      <span data-testid="cell-position">{`col:${cellPosition.colIndex},row:${cellPosition.rowIndex}`}</span>
+      <button data-testid="set-col-10" onClick={() => setColIndex?.(10)}>Set Col to 10</button>
+    </div>
+  )
+}
+
+function getDefaultDataContext({ numRows}: { numRows: number }) {
+  return {
+    data: arrayDataFrame([]),
+    key: 0,
+    version: 0,
+    maxRowNumber: 0,
+    numRows,
+  }
+}
 
 describe('CellsNavigationProvider', () => {
-  describe('when rowCount changes', () => {
-    it('resets rowIndex if out of bounds', () => {
-      const { getByTestId, rerender } = render(
-        <CellNavigationProvider colCount={5} rowCount={10} rowPadding={3}>
-          <TestComponent />
-        </CellNavigationProvider>
-      )
+  it('resets rowIndex if out of bounds when rowCount changes', () => {
+    const numberOfVisibleColumns = 4
+    const { getByTestId, rerender } = render(
+      <DataContext.Provider value={getDefaultDataContext({ numRows: 9 })}>
+        <ColumnVisibilityStatesContext.Provider value={{ numberOfVisibleColumns }}>
+          <CellNavigationProvider rowPadding={3}>
+            <RowCountComponent />
+          </CellNavigationProvider>
+        </ColumnVisibilityStatesContext.Provider>
+      </DataContext.Provider>
+    )
 
-      const cellPosition = getByTestId('cell-position')
-      const setRow10Button = getByTestId('set-row-10')
+    const cellPosition = getByTestId('cell-position')
+    const setRow10Button = getByTestId('set-row-10')
 
-      // Initially, rowIndex should be 1
-      expect(cellPosition.textContent).toBe('col:1,row:1')
+    // Initially, rowIndex should be 1
+    expect(cellPosition.textContent).toBe('col:1,row:1')
 
-      // Set rowIndex to 10
-      fireEvent.click(setRow10Button)
-      expect(cellPosition.textContent).toBe('col:1,row:10')
+    // Set rowIndex to 10
+    fireEvent.click(setRow10Button)
+    expect(cellPosition.textContent).toBe('col:1,row:10')
 
-      // Decrease rowCount to 5, which should reset rowIndex to 5
-      rerender(
-        <CellNavigationProvider colCount={5} rowCount={5} rowPadding={3}>
-          <TestComponent />
-        </CellNavigationProvider>
-      )
-      expect(cellPosition.textContent).toBe('col:1,row:5')
+    // Decrease rowCount to 5, which should reset rowIndex to 5
+    rerender(
+      <DataContext.Provider value={getDefaultDataContext({ numRows: 4 })}>
+        <ColumnVisibilityStatesContext.Provider value={{ numberOfVisibleColumns }}>
+          <CellNavigationProvider rowPadding={3}>
+            <RowCountComponent />
+          </CellNavigationProvider>
+        </ColumnVisibilityStatesContext.Provider>
+      </DataContext.Provider>
+    )
+    expect(cellPosition.textContent).toBe('col:1,row:5')
 
-      // Increase rowCount to 15, rowIndex should remain 5
-      rerender(
-        <CellNavigationProvider colCount={5} rowCount={15} rowPadding={3}>
-          <TestComponent />
-        </CellNavigationProvider>
-      )
-      expect(cellPosition.textContent).toBe('col:1,row:5')
-    })
+    // Increase rowCount to 15, rowIndex should remain 5
+    rerender(
+      <DataContext.Provider value={getDefaultDataContext({ numRows: 14 })}>
+        <ColumnVisibilityStatesContext.Provider value={{ numberOfVisibleColumns }}>
+          <CellNavigationProvider rowPadding={3}>
+            <RowCountComponent />
+          </CellNavigationProvider>
+        </ColumnVisibilityStatesContext.Provider>
+      </DataContext.Provider>
+    )
+    expect(cellPosition.textContent).toBe('col:1,row:5')
+  })
+
+  it('resets colIndex if out of bounds when colCount changes', () => {
+    const numRows = 9
+    const { getByTestId, rerender } = render(
+      <DataContext.Provider value={getDefaultDataContext({ numRows })}>
+        <ColumnVisibilityStatesContext.Provider value={{ numberOfVisibleColumns: 9 }}>
+          <CellNavigationProvider rowPadding={3}>
+            <ColCountComponent />
+          </CellNavigationProvider>
+        </ColumnVisibilityStatesContext.Provider>
+      </DataContext.Provider>
+    )
+
+    const cellPosition = getByTestId('cell-position')
+    const setCol10Button = getByTestId('set-col-10')
+
+    // Initially, colIndex should be 1
+    expect(cellPosition.textContent).toBe('col:1,row:1')
+
+    // Set colIndex to 10
+    fireEvent.click(setCol10Button)
+    expect(cellPosition.textContent).toBe('col:10,row:1')
+
+    // Decrease colCount to 5, which should reset colIndex to 5
+    rerender(
+      <DataContext.Provider value={getDefaultDataContext({ numRows })}>
+        <ColumnVisibilityStatesContext.Provider value={{ numberOfVisibleColumns: 4 }}>
+          <CellNavigationProvider rowPadding={3}>
+            <ColCountComponent />
+          </CellNavigationProvider>
+        </ColumnVisibilityStatesContext.Provider>
+      </DataContext.Provider>
+    )
+    expect(cellPosition.textContent).toBe('col:5,row:1')
+
+    // Increase rowCount to 15, rowIndex should remain 5
+    rerender(
+      <DataContext.Provider value={getDefaultDataContext({ numRows })}>
+        <ColumnVisibilityStatesContext.Provider value={{ numberOfVisibleColumns: 14 }}>
+          <CellNavigationProvider rowPadding={3}>
+            <ColCountComponent />
+          </CellNavigationProvider>
+        </ColumnVisibilityStatesContext.Provider>
+      </DataContext.Provider>
+    )
+    expect(cellPosition.textContent).toBe('col:5,row:1')
   })
 })
