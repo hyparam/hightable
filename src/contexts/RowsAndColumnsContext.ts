3b91232a9c5bd64b55d2e0c0257f9b6285d50434
Sylvain Lesage <severo@rednegra.net>
2026-01-01 15:22:40 +0100

    update doc

--- src/contexts/RowsAndColumnsContext.ts
+++ src/contexts/RowsAndColumnsContext.ts
@@ -10,8 +10,8 @@ export interface RowsRange {
 interface RowsAndColumnsContextType {
   columnsParameters?: ColumnParameters[]
   visibleRowsRange?: RowsRange // range of rows visible in the viewport
-  fetchedRowsRange?: RowsRange // range of rows fetched from the data source
-  renderedRowsRange?: RowsRange // range of rows rendered in the DOM as table rows
+  fetchedRowsRange?: RowsRange // range of rows fetched from the data source (including overscan)
+  renderedRowsRange?: RowsRange // range of rows rendered in the DOM as table rows (including padding and overscan)
   setVisibleRowsRange?: (rowsRange: RowsRange | undefined) => void
 }
 
