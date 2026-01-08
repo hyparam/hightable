a7031ea8b0c86112a43c368771c2d9cb061097af
Sylvain Lesage <severo@rednegra.net>
2026-01-08 22:48:14 +0100

    [refactor] remove unneeded code (#383)
    * [refactor] return early if not the cell to be focused
    
    * [refactor] remove unneeded code

--- src/hooks/useCellFocus.ts
+++ src/hooks/useCellFocus.ts
@@ -20,31 +20,24 @@ export function useCellFocus({ ariaColIndex, ariaRowIndex }: CellData): CellFocu
 
   // Check if the cell is the current navigation cell
   const isCurrentCell = ariaColIndex === colIndex && ariaRowIndex === rowIndex
-  const isHeaderCell = ariaRowIndex === 1 || ariaColIndex === 1
 
   const focusCellIfNeeded = useCallback((element: HTMLElement | null) => {
-    if (!element) {
+    if (!element || !isCurrentCell || !shouldFocus) {
       return
     }
     // focus on the cell when needed
     if (scrollMode === 'virtual') {
       // TODO(SL): to be implemented
     } else {
-      if (isCurrentCell && shouldFocus) {
-        if (!isHeaderCell) {
-          // scroll the cell into view
-          //
-          // scroll-padding-inline-start and scroll-padding-block-start are set in the CSS
-          // to avoid the cell being hidden by the row and column headers
-          //
-          // not applied for header cells, as they are always visible, and it was causing jumps when resizing a column
-          element.scrollIntoView({ behavior: 'auto', block: 'nearest', inline: 'nearest' })
-        }
-        element.focus()
-        setShouldFocus(false)
-      }
+      // scroll the cell into view
+      //
+      // scroll-padding-inline-start and scroll-padding-block-start are set in the CSS
+      // to avoid the cell being hidden by the row and column headers
+      element.scrollIntoView({ behavior: 'auto', block: 'nearest', inline: 'nearest' })
+      element.focus()
+      setShouldFocus(false)
     }
-  }, [isCurrentCell, isHeaderCell, shouldFocus, setShouldFocus, scrollMode])
+  }, [isCurrentCell, shouldFocus, setShouldFocus, scrollMode])
 
   // Roving tabindex: only the current navigation cell is focusable with Tab (tabindex = 0)
   // All other cells are focusable only with javascript .focus() (tabindex = -1)
