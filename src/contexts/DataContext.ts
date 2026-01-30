c5f4c034c702728d5d70c8377b952bf07bac473c
Sylvain Lesage <severo@rednegra.net>
2026-01-30 21:44:22 +0100

    add doc for the context (intellisense)

--- src/contexts/DataContext.ts
+++ src/contexts/DataContext.ts
@@ -4,10 +4,15 @@ import type { DataFrame } from '../helpers/dataframe/index.js'
 import { arrayDataFrame } from '../helpers/dataframe/index.js'
 
 interface DataContextType {
+  /** The data frame (without the number of rows, which is passed separately) */
   data: Omit<DataFrame, 'numRows'>
+  /** A key that identifies a data frame. It increments whenever a new data frame object is passed. */
   key: number
+  /** A version number that increments whenever a data frame is updated or resolved (the key remains the same). */
   version: number
+  /** The maximum number of rows to display (for row headers). Useful for filtered data. */
   maxRowNumber: number
+  /** The actual number of rows in the data frame. */
   numRows: number
 }
 
