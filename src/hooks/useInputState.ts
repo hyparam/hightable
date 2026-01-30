0bc5b4749b495cd7264a78577eb783cd0a1b08e8
Sylvain Lesage <severo@rednegra.net>
2026-01-30 15:55:40 +0100

    [refactor] split hook for type safety

--- src/hooks/useInputState.ts
+++ src/hooks/useInputState.ts
@@ -5,12 +5,21 @@ import { useCallback, useState } from 'react'
  * @param value the external value. If undefined, the input is uncontrolled and has a local state. This value cannot be unset (undefined) later if controlled, or set to a value if uncontrolled.
  * @param onChange the callback to call when the input changes. If undefined, the input is read-only.
  * @param defaultValue the default value for the local state if the input is uncontrolled.
- * @param disabled true if the input is disabled. In this case, the value is undefined and the result onChange function does nothing.
  */
 interface UseInputStateProps<T> {
   value?: T
   onChange?: ((value: T) => void)
-  defaultValue?: T
+  defaultValue: T
+}
+
+/**
+ * Props for the useInputOrDisabledState hook.
+ * @param value the external value. If undefined, the input is uncontrolled and has a local state. This value cannot be unset (undefined) later if controlled, or set to a value if uncontrolled.
+ * @param onChange the callback to call when the input changes. If undefined, the input is read-only.
+ * @param defaultValue the default value for the local state if the input is uncontrolled.
+ * @param disabled true if the input is disabled. In this case, the value is undefined and the result onChange function does nothing.
+ */
+interface UseInputOrDisabledStateProps<T> extends UseInputStateProps<T> {
   disabled?: boolean
 }
 
@@ -21,8 +30,49 @@ interface UseInputStateProps<T> {
  * @param onChange the callback to call when the input changes. undefined if the input cannot be changed by the user.
  */
 interface UseInputStateResult<T> {
-  value?: T
+  value: T
   onChange?: ((value: T) => void)
+}
+
+/**
+ * Simulates the state of React <input> components. See https://react.dev/reference/react-dom/components/input#controlling-an-input-with-a-state-variable
+ *
+ * The input state can be:
+ * - controlled (if value is defined): the parent controls the value. No local state.
+ * - uncontrolled (if value is undefined): the input controls the value. Local state.
+ *
+ * Note that the onChange prop can be defined or undefined. If undefined in a controlled state, the input is read-only (no interactions),
+ * else, the input can change but the parent cannot get the value.
+ */
+export function useInputState<T>({ value: propValue, onChange: propOnChange, defaultValue }: UseInputStateProps<T>): UseInputStateResult<T> {
+  const [initialValue] = useState<T | undefined>(propValue)
+
+  // for uncontrolled inputs
+  const [localValue, setLocalValue] = useState<T>(defaultValue)
+  const onChange = useCallback((selection: T) => {
+    propOnChange?.(selection)
+    setLocalValue(selection)
+  }, [propOnChange])
+
+  // The input is forever in one of these two modes:
+
+  // - controlled (no local state)
+  if (initialValue !== undefined) {
+    if (propValue === undefined) {
+      console.warn('The value is controlled (it has no local state) because the property was initially defined. It cannot be set to undefined now (it is set back to the initial value).')
+    }
+    return {
+      value: propValue ?? initialValue,
+      // read-only if onChange is undefined
+      onChange: propOnChange,
+    }
+  }
+
+  // - uncontrolled (local state)
+  if (propValue !== undefined) {
+    console.warn('The value is uncontrolled (it only has a local state) because the property was initially undefined. It cannot be set to a value now and is ignored.')
+  }
+  return { value: localValue, onChange }
 }
 
 /**
@@ -36,40 +86,8 @@ interface UseInputStateResult<T> {
  * Note that the onChange prop can be defined or undefined. If undefined in a controlled state, the input is read-only (no interactions),
  * else, the input can change but the parent cannot get the value.
  */
-export function useInputState<T>({ value, onChange, defaultValue, disabled }: UseInputStateProps<T>): UseInputStateResult<T> {
-  const [initialValue] = useState<T | undefined>(value)
-  const [isControlled] = useState<boolean>(value !== undefined)
+export function useInputOrDisabledState<T>({ value, onChange, defaultValue, disabled }: UseInputOrDisabledStateProps<T>): UseInputStateResult<T> | undefined {
   const [isDisabled] = useState<boolean>(disabled ?? false)
-
-  // for uncontrolled inputs
-  const [localValue, setLocalValue] = useState<T | undefined>(defaultValue)
-  const uncontrolledOnChange = useCallback((selection: T) => {
-    onChange?.(selection)
-    setLocalValue(selection)
-  }, [onChange])
-
-  // disabled
-  if (isDisabled) {
-    return {}
-  }
-
-  // The input is forever in one of these two modes:
-
-  // - controlled (no local state)
-  if (isControlled) {
-    if (value === undefined) {
-      console.warn('The value is controlled (it has no local state) because the property was initially defined. It cannot be set to undefined now (it is set back to the initial value).')
-    }
-    return {
-      value: value ?? initialValue,
-      // read-only if onChange is undefined
-      onChange,
-    }
-  }
-
-  // - uncontrolled (local state)
-  if (value !== undefined) {
-    console.warn('The value is uncontrolled (it only has a local state) because the property was initially undefined. It cannot be set to a value now and is ignored.')
-  }
-  return { value: localValue, onChange: uncontrolledOnChange }
+  const inputState = useInputState<T>({ value, onChange, defaultValue })
+  return isDisabled ? undefined : inputState
 }
