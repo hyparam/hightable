e68cadcded5cefb3a37ccc3e1e24f54e782898f1
Sylvain Lesage <severo@rednegra.net>
2026-02-17 11:15:43 +0100

    Reduce rerenders (#436)
    * memoize prop
    
    * avoid rerendering due to prop change
    
    * avoid creating a new object on each rerender
    
    * use 'version' as the key for Slice
    
    * no need to embed in an object
    
    * copilot comment
    
    * Revert "no need to embed in an object"
    
    This reverts commit e3c226390f5d495213e4f82417189c431966e2a5.
    
    * Revert "use 'version' as the key for Slice"
    
    This reverts commit d0b4af76806ac09c628de782d33ffb5c87e29bd9.

--- src/components/TableHeader/TableHeader.tsx
+++ src/components/TableHeader/TableHeader.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react'
+import { useMemo } from 'react'
 
 import type { ColumnParameters } from '../../contexts/ColumnParametersContext.js'
 import { ariaOffset } from '../../helpers/constants.js'
@@ -21,21 +21,24 @@ interface TableHeaderProps {
 export default function TableHeader({
   columnsParameters, orderBy, setOrderBy, canMeasureColumn, ariaRowIndex, exclusiveSort,
 }: TableHeaderProps) {
-  // Function to handle click for changing orderBy
-  const getToggleOrderBy = useCallback((columnHeader: string) => {
-    if (!setOrderBy || !orderBy) return undefined
-    return () => {
-      const next = exclusiveSort ? toggleColumnExclusive(columnHeader, orderBy) : toggleColumn(columnHeader, orderBy)
-      setOrderBy(next)
-    }
-  }, [orderBy, setOrderBy, exclusiveSort])
+  const toggleOrderBys = useMemo(() => {
+    return Object.fromEntries(columnsParameters.map(({ name }) => {
+      const toggleOrderBy = (!setOrderBy || !orderBy)
+        ? undefined
+        : () => {
+            const next = exclusiveSort ? toggleColumnExclusive(name, orderBy) : toggleColumn(name, orderBy)
+            setOrderBy(next)
+          }
+      return [name, toggleOrderBy]
+    }))
+  }, [columnsParameters, orderBy, setOrderBy, exclusiveSort])
 
   const orderByColumn = useMemo(() => {
     return new Map((orderBy ?? []).map(({ column, direction }, index) => [column, { direction, index }]))
   }, [orderBy])
 
   return columnsParameters.map((columnParameters, visibleColumnIndex) => {
-    const { name, index: columnIndex, className, ...columnConfig } = columnParameters
+    const { name, index: columnIndex, className } = columnParameters
     // Note: columnIndex is the index of the column in the dataframe header
     // and not the index of the column in the table (which can be different if
     // some columns are hidden, or if the order is changed)
@@ -47,13 +50,13 @@ export default function TableHeader({
         direction={orderByColumn.get(name)?.direction}
         orderByIndex={orderByColumn.get(name)?.index}
         orderBySize={orderBy?.length}
-        toggleOrderBy={getToggleOrderBy(name)}
+        toggleOrderBy={toggleOrderBys[name]}
         columnName={name}
         columnIndex={columnIndex}
         className={className}
         ariaColIndex={visibleColumnIndex + ariaOffset}
         ariaRowIndex={ariaRowIndex}
-        columnConfig={columnConfig}
+        columnConfig={columnParameters}
       >
         {name}
       </ColumnHeader>
