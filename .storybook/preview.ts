6b7da51b93ad9908340142fa241600a87a5eb5c1
Sylvain Lesage <severo@rednegra.net>
2026-03-02 17:48:32 +0100

    add React Scan to storybook
    Note that it's only enabled in "isolation mode": https://github.com/aidenybai/react-scan/issues/285#issuecomment-2707163571

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,12 @@
 import './global.css'
 
 import type { Preview } from '@storybook/react-vite'
+import { scan } from 'react-scan'
+
+scan({
+  enabled: true,
+  log: true,
+})
 
 const preview: Preview = {
   parameters: {
