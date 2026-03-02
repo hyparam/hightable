38f799e79eebd8c37089e8baef216d160127b9e0
Sylvain Lesage <severo@rednegra.net>
2026-03-02 18:01:17 +0100

    explicitly run React Scan in isolation mode only

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,10 +3,14 @@ import './global.css'
 import type { Preview } from '@storybook/react-vite'
 import { scan } from 'react-scan'
 
-scan({
-  enabled: true,
-  log: true,
-})
+// Only run the scan in Storybook in isolation mode
+// (it does not work in normal mode, see https://github.com/aidenybai/react-scan/issues/285#issuecomment-2707163571)
+if (typeof window !== 'undefined' && window.top === window.self) {
+  scan({
+    enabled: true,
+    log: true,
+  })
+}
 
 const preview: Preview = {
   parameters: {
