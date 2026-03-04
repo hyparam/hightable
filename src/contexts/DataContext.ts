8d518d703d0370e2ec9d04ba75ba62823cd12d12
Sylvain Lesage <severo@rednegra.net>
2026-03-04 10:25:59 +0100

    use ColumnDescriptor type

--- src/contexts/DataContext.ts
+++ src/contexts/DataContext.ts
@@ -1,11 +1,11 @@
 import { createContext, useContext } from 'react'
 
-export type ColumnDescriptorContextValue = { name: string; sortable?: boolean }
+import type { ColumnDescriptor } from '../helpers/dataframe/types.js'
 
 export const DataKeyContext = createContext<number>(0)
 export const DataVersionContext = createContext<number>(0)
 export const NumRowsContext = createContext<number>(0)
-export const ColumnDescriptorsContext = createContext<ColumnDescriptorContextValue[]>([])
+export const ColumnDescriptorsContext = createContext<Pick<ColumnDescriptor, 'name' | 'sortable'>[]>([])
 export const NumColumnsContext = createContext<number>(0)
 
 // the data key is only used in tests
